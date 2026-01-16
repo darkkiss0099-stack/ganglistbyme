32079e7ac6a75bafa3cc5e35b8ebb64d304b1d4c
drakegg <darkkiss0099@gmail.com>
2026-01-17 01:59:44 +0700

    data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const gangs = [
+  { id: 1, name: "Ballas", description: "Street gang from Los Santos", tag: "ballas" },
+  { id: 2, name: "Vagos", description: "West Coast gang with green colors", tag: "vagos" },
+  { id: 3, name: "Families", description: "Community gang, respected in city", tag: "families" }
+];
